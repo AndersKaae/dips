7b88cf4f636bd403e6cba6072463b1ea88ae2b00
Anders Lyager Kaae <anders@anderss-mbp-2.lan>
2020-06-17 19:14:06 +0200

    Bug fix

--- daemon.py
+++ daemon.py
@@ -5,19 +5,26 @@ from autobrowse import selenium
 import time
 from datetime import datetime
 
-firstRun = True
-
-while True:
-    if firstRun != True:
-        print('Waiting for 20 minutes to fetch report...')
-        time.sleep(1200)
-    print('Getting data with Selenium')
-    selenium("https://payment.architrade.com/login/login.action")
-    print('Waiting for 10 minutes for report to arrive...')
-    time.sleep(600)
+def ProcessReports():
     with os.scandir('reports/') as entries:
         for entry in entries:
-            ReadData(os.path.join(os.path.dirname(__file__), "reports/", entry.name))
-            os.remove("ChangedFile.csv")
-    firstRun = False
-    SetLastUpdate(datetime.today().strftime("%d-%m-%Y %H:%M"))
+            fileandpath = os.path.join(os.path.dirname(__file__), "reports/", entry.name)
+            ReadData(fileandpath)
+            os.remove(fileandpath)
+
+def RunDaemon():
+    firstRun = True
+    while True:
+        if firstRun != True:
+            print(str(datetime.today().strftime("%d-%m-%Y %H:%M")) + ': Waiting for 20 minutes to fetch report...')
+            time.sleep(1200)
+        print('Getting data with Selenium')
+        selenium("https://payment.architrade.com/login/login.action")
+        print(str(datetime.today().strftime("%d-%m-%Y %H:%M")) + ': Waiting for 10 minutes for report to arrive...')
+        time.sleep(600)
+        ProcessReports()
+        firstRun = False
+        SetLastUpdate(datetime.today().strftime("%d-%m-%Y %H:%M"))
+
+#ProcessReports()
+RunDaemon()
